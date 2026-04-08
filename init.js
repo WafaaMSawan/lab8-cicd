9e76adbe90dc1d1d5391424810b1858adc001d91
WafaaMSawan <wafaa98511@gmail.com>
2026-04-09 01:57:17 +0200

    Add Tea

--- init.js
+++ init.js
@@ -1,6 +1,7 @@
 db = db.getSiblingDB("tasksdb");
 
 db.tasks.insertMany([
+  { id:7, name:"Tea", status:"pending"},
   { id:1, name:"Study", status:"pending"},
   { id:2, name:"Gym", status:"done"},
   { id:3, name:"Shopping", status:"pending"}
